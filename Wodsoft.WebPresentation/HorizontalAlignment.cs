5db8adee5f4ddfb59262760df6ace22bf1b0324a
Kation <lwcqq@163.com>
2016-08-07 18:48:50 +0800

    添加水平垂直对齐与坐标

---
+++ Wodsoft.WebPresentation/HorizontalAlignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wodsoft.Web
+{
+    public enum HorizontalAlignment
+    {
+        Left, Center, Right, Stretch
+    }
+}
